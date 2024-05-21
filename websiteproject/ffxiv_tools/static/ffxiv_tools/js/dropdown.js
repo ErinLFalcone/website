862760ac13af95a81609be33635aa801b937e980
ELFalcone <erinlfalcone@gmail.com>
2024-05-21 14:50:24 -0700

    Dropdown menus open and close

--- websiteproject/ffxiv_tools/static/ffxiv_tools/js/dropdown.js
+++ websiteproject/ffxiv_tools/static/ffxiv_tools/js/dropdown.js
@@ -1,11 +1,34 @@
-function dropMenuFunction(dropdownId) {
-    document.getElementById(`${dropdownId}`).classList.toggle("drop-click")
+const dropDownClasses = ['item-all-drop', 'item1-drop', 'item2-drop'];
+var dropQueryAll = document.querySelectorAll('.drop-container');
+
+// Toggles dropdown menu when button is clicked on
+function dropMenuToggle(dropdownId) {
+    document.getElementById(`${dropdownId}`).classList.toggle("block-display")
 }
 
+// Hides all dropdown containers except the one identified by ID with attribute
+function dropMenuClose(dropdownToKeep) {
+    var dropdownHideList = document.querySelectorAll(`.drop-container:not(#${dropdownToKeep})`);
+    dropdownHideList.forEach((item) => item.classList.remove("block-display"));
+}
+
+// Listens for clicks, opens dropdowns when button clicked, hides when clicked outside
+document.addEventListener('click', function(e) {
+    // Opens and closes item dropdown menus
+    if (dropDownClasses.includes(e.target.id.slice(0,-7))) {
+        dropMenuToggle(`${e.target.id.slice(0,-7)}`);
+        dropMenuClose(`${e.target.id.slice(0,-7)}`);
+    // Hides dropdown menus when clicked outside of
+    } else if (!e.target.classList.contains("drop-choice")) {
+        dropMenuClose("dummy-class");
+    };
+}, false);
+
+// Shows and hides items based on buttons clicked in dropdown
 function dropSelectFunction(dropMenuClass, dropSelectionClass) {
-    var hideList = document.querySelectorAll(`.${dropMenuClass}`);
-    hideList.forEach((item) => item.style.display="none");
+    var selectHideList = document.querySelectorAll(`.${dropMenuClass}`);
+    selectHideList.forEach((item) => item.style.display="none");
     
     var selectList = document.querySelectorAll(`${dropSelectionClass}`);
     selectList.forEach((item) => item.style.display="block");
-}
+};
