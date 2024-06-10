a2793f100f1122a513fd5b979e84247fc23a7dcb
ELFalcone <erinlfalcone@gmail.com>
2024-06-10 15:02:47 -0700

    Second calculation working, bug fixes

--- websiteproject/ffxiv_tools/static/ffxiv_tools/js/calculation.js
+++ websiteproject/ffxiv_tools/static/ffxiv_tools/js/calculation.js
@@ -1,10 +1,16 @@
 // Function to update item counters, called in dragdrop.js
 var itemList = ["Milk", "Fur", "Egg", "Carapace", "Feather", "Claw", "Horn", "Fang", "Fleece"];
 
+function itemNumUpdate(item, slot, mult) {
+    const itemToUpdate = document.getElementById(`${item}-count-${slot}`);
+    const numOfItem = document.querySelectorAll(`div.selected-animals .${item}-${slot}`);
+    itemToUpdate.innerHTML = (numOfItem.length * mult);
+}
+
 function itemCountUpdate() {
     for (let i = 0; i < itemList.length; i++) {
-        const itemNumToUpdate = document.getElementById(`${itemList[i]}-number`);
-        const numOfItem = document.querySelectorAll(`div.selected-animals .${itemList[i]}1`);
-        itemNumToUpdate.innerHTML = numOfItem.length;
+        const multNum = document.querySelector("tr.feed-choice:not(.none-display) td");
+        itemNumUpdate(itemList[i], "one", 1);
+        itemNumUpdate(itemList[i], "two", parseFloat(multNum.innerHTML));
     };
 };
